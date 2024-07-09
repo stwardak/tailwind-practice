6273fc6c87c0760fa1ed63dd1103955aa866b771
stwardak <148924249+stwardak@users.noreply.github.com>
2024-07-09 15:06:00 -0500

    styling update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Content } from './Content'
 function App() {
 
   return (
-    <div className= "flex min-h-screen flex-col">
+    <div className= "flex min-h-screen flex-col font-mono">
       <Header/>
       <div className= "container mx-auto my-20 p-4">
       <Content/>
--- src/Content.jsx
+++ src/Content.jsx
@@ -13,24 +13,40 @@ export function Content() {
 
     {
       name: "King",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel iusto, explicabo perferendis quod sapiente impedit consectetur in voluptate ad animi eveniet corrupti veniam sit illum dolore similique adipisci iure porro?",
       image: "https://www.reptilecentre.com/cdn/shop/articles/2946733f1f65584baa93e7509e72f011.jpg?v=1718288664"
     },
     {
       name: "Bearded Dragon",
-      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum, voluptate! Nesciunt sit, non corrupti deserunt deleniti at repellat, ut asperiores molestiae fuga vero ullam itaque veniam sequi, quibusdam obcaecati incidunt?",
+      description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore sit numquam veritatis sequi laudantium, labore quas, explicabo, ipsam praesentium sed harum fugiat eveniet fuga reiciendis? In itaque inventore quo aliquam.",
       image: "https://s7d2.scene7.com/is/image/PetSmart/ARHERO-ASetUpGuideForNewBeardedDragonParents-20160818?$AR0301$"
     },
     {
       name: "Chameleon",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint placeat odio molestias, nemo nihil enim qui eligendi aut repudiandae sit magnam distinctio mollitia, maiores libero voluptatibus perferendis? Natus, aut pariatur!",
       image: "https://news.northwestern.edu/assets/Stories/2018/06/chameleon-odom-web__FocusFillWzY0NCwzMzgsInkiLDEyXQ.jpg"
     },
 
     {
       name: "Komodo Dragon",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quae hic consequatur odit, fugit quaerat, assumenda nam blanditiis quisquam possimus ipsum facilis voluptas? Illo quasi facere incidunt vitae eaque quis.",
       image: "https://media.cntraveler.com/photos/590b484796ac4049cc0edcc0/16:9/w_2560%2Cc_limit/GettyImages-200253900-001.jpg"
+    },
+    {
+      name: "Geico",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum, voluptate! Nesciunt sit, non corrupti deserunt deleniti at repellat, ut asperiores molestiae fuga vero ullam itaque veniam sequi, quibusdam obcaecati incidunt?",
+      image: "https://discovery.sndimg.com/content/dam/images/discovery/fullset/2022/3/11/GettyImages-521447418.jpg.rend.hgtvcom.616.462.suffix/1648494137294.jpeg"
+    },
+    {
+      name: "Blue-Crested",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam amet illo blanditiis commodi, ut enim non atque. Odio reiciendis ratione dignissimos laboriosam aspernatur ducimus saepe quas maxime, ex nulla quaerat.",
+      image: "https://i0.wp.com/www.australiangeographic.com.au/wp-content/uploads/2021/09/blue-crested-lizard-1.jpg?fit=2500%2C1374&ssl=1"
+    },
+
+    {
+      name: "Yellow-Spotted",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod malesuada.",
+      image: "https://images.articlesfactory.com/750x0/fb0ffc4b-7b53-470b-81e7-3c70b1f50427.webp"
     },
   ];
 
@@ -39,10 +55,10 @@ export function Content() {
       <h1 className="text-8xl font-serif flex justify-center mb-10">Lizards</h1>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
         {lizards.map((lizard, index) => (
-          <div key={index} className="border border-gray- p-4 shadow-lg mb-8">
+          <div key={index} className="border border-gray- p-2 shadow-lg mb-8 bg-white  ">
             <h2 className="text-4xl font-bold text-center mb-4">{lizard.name}</h2>
-            <p className="text-gray-800 px-5 py-10 m-10 text-center">{lizard.description}</p>
-            <img src={lizard.image} alt={lizard.name} className="w-1/2 h-auto mx-auto p-4"/>
+            <p className="text-gray-800 px-2 py-10 m-2 text-center">{lizard.description}</p>
+            <img src={lizard.image} alt={lizard.name} className="w-8/12 h-auto mx-auto p-4"/>
           </div>
         ))}
       </div>
